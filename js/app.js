4d31ae80ea582ed5de190fca7784cb37ad471e2f
Andrey Lysov <hryakandrey@gmail.com>
2018-04-12 20:04:01 +0300

    refactor: Add code comments and optimize redundant functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// @description Shuffle function from http://stackoverflow.com/a/2450976
+// @param {array}
+// @returns {array}
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -15,9 +17,6 @@ function shuffle(array) {
 }
 
 
-/*
- * Create a list that holds all of your cards
- */
 
 const cards = [...document.getElementsByClassName('card')];
 const deck = document.querySelector('.deck');
@@ -29,104 +28,15 @@ let openedCards = [];
 let matchCounter = 0;
 let counter = 0;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
+
+// @description Shuffles cards and reset counters
 function startNewGame(){
   shuffle(cards);
   for (const card of cards) {
     card.classList.remove('open', 'show', 'match', 'disable');
     deck.appendChild(card);
   }
-  resetCounters();
-}
-
-
-
-function clickedCard(){
-  if (! this.classList.contains('disable') && ! this.classList.contains('match')){
-    this.classList.add('open', 'show', 'disable');
-    check(this);
-  }
-}
-
-
-function disable(){
-  for (const card of cards) {
-    card.classList.add('disable');
-  }
-}
-
-
-function enable(){
-  for (const card of cards) {
-    card.classList.remove('disable');
-  }
-}
-
-
-
-function check(card){
-  openedCards.push(card);
-  if (openedCards.length === 2) {
-    disable();
-    if (openedCards[0].firstElementChild.className === openedCards[1].firstElementChild.className) {
-      cardsMatch();
-    } else {
-      cardsUnmatch();
-    }
-  }
-}
-
-function cardsMatch(){
-  openedCards[0].classList.add('match');
-  openedCards[0].classList.remove('open', 'show');
-  openedCards[1].classList.add('match');
-  openedCards[1].classList.remove('open', 'show');
-  openedCards = [];
-  addMatch();
-}
-
-function cardsUnmatch(){
-  setTimeout(function(){
-    openedCards[0].classList.remove('open', 'show', 'disable');
-    openedCards[1].classList.remove('open', 'show', 'disable');
-    openedCards = [];
-    addMove();
-  }, 1000);
-}
-
-
-function addMatch(){
-  matchCounter++;
-  if (matchCounter == 8) {
-    console.log('finish');
-  } else {
-    console.log('play more');
-  }
-  addMove();
-}
-
-
-function addMove(){
-  counter++;
-  moves.innerText = counter;
-  if (counter == 10){
-    stars[2].style.visibility = 'hidden';
-  } else if (counter == 20) {
-    stars[1].style.visibility = 'hidden';
-  } else if (counter == 30) {
-    stars[0].style.visibility = 'hidden';
-  }
-  enable();
-}
-
-
-function resetCounters(){
   matchCounter = 0;
   counter = 0;
   moves.innerText = counter;
@@ -137,27 +47,89 @@ function resetCounters(){
 
 
 
+// @description Disallows gameplay on cards with 'disable' and 'match' classes
+function clickedCard(){
+  if (! this.classList.contains('disable') && ! this.classList.contains('match')){
+    this.classList.add('open', 'show', 'disable');
+    check(this);
+  }
+}
+
+
+
+// @description Checks if two opened cards match
+// @param {object} a clicked card to be checked for matching
+function check(card){
+  openedCards.push(card);
+  if (openedCards.length === 2) {
+    for (const card of cards) {
+      card.classList.add('disable');
+    }
+    if (openedCards[0].firstElementChild.className === openedCards[1].firstElementChild.className) {
+      cardsMatch();
+    } else {
+      cardsUnmatch();
+    }
+  }
+}
+
+// @description Adds 'match' class to cards
+function cardsMatch(){
+  openedCards[0].classList.add('match');
+  openedCards[0].classList.remove('open', 'show');
+  openedCards[1].classList.add('match');
+  openedCards[1].classList.remove('open', 'show');
+  openedCards = [];
+  addMatch();
+}
+
+
+// @description Removes classes from unmatched cards with delay
+function cardsUnmatch(){
+  setTimeout(function(){
+    openedCards[0].classList.remove('open', 'show', 'disable');
+    openedCards[1].classList.remove('open', 'show', 'disable');
+    openedCards = [];
+    addMove();
+  }, 1000);
+}
+
+// @description Calls for modal
+function addMatch(){
+  matchCounter++;
+  if (matchCounter == 8) {
+    console.log('finish');
+  } else {
+    console.log('play more');
+  }
+  addMove();
+}
+
+// @description Counts moves and hides stars
+function addMove(){
+  counter++;
+  moves.innerText = counter;
+  if (counter == 10){
+    stars[2].style.visibility = 'hidden';
+  } else if (counter == 20) {
+    stars[1].style.visibility = 'hidden';
+  } else if (counter == 30) {
+    stars[0].style.visibility = 'hidden';
+  }
+  for (const card of cards) {
+    card.classList.remove('disable');
+  }
+}
+
+
+
+// Adds event listeners to all cards
 for (const card of cards) {
   card.addEventListener('click', clickedCard);
 }
 
+// Adds event listener restart button
 restart.addEventListener('click', startNewGame);
 
-
-
+// Starts a new game when the document is loaded
 window.onload = startNewGame();
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
