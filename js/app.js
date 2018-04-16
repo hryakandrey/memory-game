6887956809e7aa9d9753e8231c5a608e4876ee5d
Andrey Lysov <hryakandrey@gmail.com>
2018-04-16 15:23:01 +0300

    refactor: Optimize timer function

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ function shuffle(array) {
 }
 
 
-
 const cards = [...document.getElementsByClassName('card')];
 const deck = document.querySelector('.deck');
 const moves = document.querySelector('.moves');
@@ -36,27 +35,29 @@ let openedCards = [];
 let matchCounter = 0;
 let counter = 0;
 let starCounter = 3;
-let timer = 0;
 let finalTime;
+let timerId;
 
 
 // @description Shuffles cards and reset counters
 function startNewGame(){
   shuffle(cards);
+
   for (const card of cards) {
     card.classList.remove('open', 'show', 'match', 'disable');
     deck.appendChild(card);
   }
+
   for (star of stars) {
     star.style.removeProperty('visibility');
   }
+
   matchCounter = 0;
   counter = 0;
   moves.innerText = counter;
-  timer = 0;
   time.innerText = "0 min 0 sec";
+  startTimer();
 }
-
 
 
 // @description Disallows gameplay on cards with 'disable' and 'match' classes
@@ -66,7 +67,6 @@ function clickedCard(){
     check(this);
   }
 }
-
 
 
 // @description Checks if two opened cards match
@@ -84,6 +84,7 @@ function check(card){
     }
   }
 }
+
 
 // @description Adds 'match' class to cards
 function cardsMatch(){
@@ -103,19 +104,22 @@ function cardsUnmatch(){
     openedCards[1].classList.remove('open', 'show', 'disable');
     openedCards = [];
     addMove();
-  }, 1000);
+  }, 500);
 }
+
 
 // @description Calls for modal
 function addMatch(){
   matchCounter++;
   if (matchCounter == 8) {
+    clearInterval(timerId);
     finalTime = time.innerText;
     setTimeout(gameOver, 1000);
   } else {
     addMove();
   }
 }
+
 
 // @description Counts moves and hides stars, makes cards clickable
 function addMove(){
@@ -140,15 +144,23 @@ function addMove(){
 function gameOver(){
   finalscore.innerText = counter;
   starrating.innerText = starCounter;
-  finaltime.innerText = finalTime;
+  finaltime.innerText = time.innerText;
 
   if (counter >= 20) {
     result.innerText = 'Looser';
     graph.style.backgroundImage = 'url(img/looser.png)';
   }
-  
+
   overlay.classList.add('show');
 }
+
+
+// @description Stops timer and starts new game
+function reStart(){
+  clearInterval(timerId);
+  startNewGame();
+}
+
 
 // @description Closes game over dialog and starts new game
 function rePlay(){
@@ -156,23 +168,27 @@ function rePlay(){
   startNewGame();
 }
 
+
+// @description Updates timer info with 1 second delay
+function startTimer(){
+  let timer = 0;
+  timerId = setInterval(function(){
+    timer++;
+    time.innerText = `${Math.round(timer/60)} min ${timer%60} sec`;
+  }, 1000);
+}
+
+
 // Adds event listeners to all cards
 for (const card of cards) {
   card.addEventListener('click', clickedCard);
 }
 
 // Adds event listener to restart button
-restart.addEventListener('click', startNewGame);
+restart.addEventListener('click', reStart);
 
 // Adds event listener to play again button
 replayButton.addEventListener('click', rePlay);
-
-// @description Updates timer info with 1 second delay
-let timerId = setTimeout(function tick() {
-  timer++;
-  time.innerText = `${Math.round(timer/60)} min ${timer%60} sec`;
-  timerId = setTimeout(tick, 1000);
-}, 1000);
 
 // Starts a new game when the document is loaded
 window.onload = startNewGame();
